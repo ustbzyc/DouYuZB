76ebe2404026f2abb5362c032b8c50544ede2377
kfzx-zhangyc <zhangyc@sdc.icbc.com.cn>
2020-03-10 22:52:13 +0800

    初始化项目

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by songya on 2020/3/10.
+//  Copyright © 2020 songya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
